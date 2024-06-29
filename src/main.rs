6b31b7c903ad704444e37de9dfe449aef16aa0cb
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-06-29 12:02:59 +0700

    actions!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::io::stdin;
-use std::time::Duration;
-use std::path::PathBuf;
 use clap::Parser;
 use dbus::blocking::Connection;
+use std::fs::File;
+use std::io::{stdin, stdout, Error, Result, Write};
+use std::path::{Path, PathBuf};
+use std::process::{exit, Command, Stdio};
+use std::time::Duration;
 
 mod dbusgen;
 use dbusgen::login1::OrgFreedesktopLogin1Manager;
@@ -15,7 +17,7 @@ struct Args {
 	images: PathBuf,
 	/// Path to the audio file
 	audio: PathBuf,
-	
+
 	/// FPS as divisor of 29.97
 	#[arg(long, default_value_t = 2)]
 	fps: u8,
@@ -30,29 +32,71 @@ struct Args {
 	pixel_size: u16,
 }
 
-// fn inhibit() {}
-
-fn main() {
-    let _ = Args::parse();
-
-	let system_bus = Connection::new_system().unwrap();
-
-	let login1 = system_bus.with_proxy(
+fn inhibit(bus: Connection) -> dbus::arg::OwnedFd {
+	let login1 = bus.with_proxy(
 		"org.freedesktop.login1",
 		"/org/freedesktop/login1",
-		Duration::new(5, 0)
+		Duration::new(5, 0),
 	);
-	
-	let inhibitor = login1.inhibit(
-		"sleep",
-		"Bad Apple!!",
-		"Playing Bad Apple!!",
-		"block"
-	).unwrap();
 
-	println!("press enter to stop inhibiting");
+	return login1
+		.inhibit("sleep", "Bad Apple!!", "Playing Bad Apple!!", "block")
+		.unwrap();
+}
+
+fn actions() {
+	println!("Actions:");
+	println!("1. Create compositors and start Bad Apple!!");
+	println!("2. Start Bad Apple!! with existing compositors");
+	println!("3. Start compositors only");
+	println!("4. Kill stray processes");
+
+	print!("Your action? ");
+	stdout().flush().unwrap();
+
 	let mut input = String::new();
 	stdin().read_line(&mut input).unwrap();
 
-	drop(inhibitor);
+	match input.trim() {
+		"1" => {
+			println!("Creating compositors and starting Bad Apple!!");
+		}
+		"2" => {
+			println!("Starting Bad Apple!! with existing compositors");
+		}
+		"3" => {
+			println!("Starting compositors only");
+			return;
+		}
+		"4" => {
+			println!("Killing stray processes");
+			return;
+		}
+		_ => {
+			println!("I can break rules too. Goodbye.");
+			exit(1);
+		}
+	}
+
+	start_play();
+}
+
+fn start_play() {
+	print!("Press enter to start Bad Apple!! ");
+	stdout().flush().unwrap();
+	let mut _i = String::new();
+	stdin().read_line(&mut _i).unwrap();
+}
+
+fn main() -> Result<()> {
+	// let _ = Args::parse();
+	// let inhibitor = inhibit(Connection::new_system().unwrap());
+
+	// actions();
+
+	// drop(inhibitor);
+
+	// launch_dbus()?;
+
+	Ok(())
 }
